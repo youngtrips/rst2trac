1a51d913b539bee186a7df511e523b47bd77df49
youngtrips <youngtrips@180d60f8-1712-8566-910b-241bcd67f2e5>
2011-03-18 07:43:28 +0000

    update rst2trac.py with supporting sub directories, adding author information for wiki page and changging method to create wikipage

--- rst2trac.py
+++ rst2trac.py
@@ -7,6 +7,7 @@
 # Description: 
 #
 
+import tempfile
 import string
 import sys
 import os
@@ -18,136 +19,7 @@ import trac.env
 import trac.wiki
 import trac.wiki.admin
 
-import sys
-from docutils.nodes import SparseNodeVisitor, paragraph, title_reference, \
-    emphasis
-from docutils.writers import Writer
-from docutils.core import publish_string
-
-trac_path = "/trac/test"
-rst_path = "test/trunk/doc"
-
-##################################################################################
-# copy from rst2wiki
-# site: http://code.google.com/p/python-nose
-##################################################################################
-
-class WikiWriter(Writer):
-    def translate(self):
-        visitor = WikiVisitor(self.document)
-        self.document.walkabout(visitor)
-        self.output = visitor.astext()
-
-class WikiVisitor(SparseNodeVisitor):
-
-    def __init__(self, document):
-        SparseNodeVisitor.__init__(self, document)
-        self.list_depth = 0
-        self.list_item_prefix = None
-        self.indent = self.old_indent = ''
-        self.output = []
-        self.preformat = False
-        
-    def astext(self):
-        #return '\n>>>\n\n'+ ''.join(self.output) + '\n\n<<<\n'
-        return ''.join(self.output)
-
-    def visit_Text(self, node):
-        #print "Text", node
-        data = node.astext()
-        if not self.preformat:
-            data = data.lstrip('\n\r')
-            data = data.replace('\r', '')
-            data = data.replace('\n', ' ')
-        self.output.append(data)
-    
-    def visit_bullet_list(self, node):
-        self.list_depth += 1
-        self.list_item_prefix = (' ' * self.list_depth) + '* '
-
-    def depart_bullet_list(self, node):
-        self.list_depth -= 1
-        if self.list_depth == 0:
-            self.list_item_prefix = None
-        else:
-            (' ' * self.list_depth) + '* '
-        self.output.append('\n\n')
-                           
-    def visit_list_item(self, node):
-        self.old_indent = self.indent
-        self.indent = self.list_item_prefix
-
-    def depart_list_item(self, node):
-        self.indent = self.old_indent
-        
-    def visit_literal_block(self, node):
-        self.output.extend(['{{{', '\n'])
-        self.preformat = True
-
-    def depart_literal_block(self, node):
-        self.output.extend(['\n', '}}}', '\n\n'])
-        self.preformat = False
-        
-    def visit_paragraph(self, node):
-        self.output.append(self.indent)
-        
-    def depart_paragraph(self, node):
-        self.output.append('\n\n')
-        if self.indent == self.list_item_prefix:
-            # we're in a sub paragraph of a list item
-            self.indent = ' ' * self.list_depth
-        
-    def visit_reference(self, node):
-        if node.has_key('refuri'):
-            href = node['refuri']
-        elif node.has_key('refid'):
-            href = '#' + node['refid']
-        else:
-            href = None
-        self.output.append('[' + href + ' ')
-
-    def depart_reference(self, node):
-        self.output.append(']')
-
-    def visit_subtitle(self, node):
-        self.output.append('=== ')
-
-    def depart_subtitle(self, node):
-        self.output.append(' ===\n\n')
-        self.list_depth = 0
-        self.indent = ''
-        
-    def visit_title(self, node):
-        self.output.append('== ')
-
-    def depart_title(self, node):
-        self.output.append(' ==\n\n')
-        self.list_depth = 0
-        self.indent = ''
-        
-    def visit_title_reference(self, node):
-        self.output.append("`")
-
-    def depart_title_reference(self, node):
-        self.output.append("`")
-
-    def visit_emphasis(self, node):
-        self.output.append('*')
-
-    def depart_emphasis(self, node):
-        self.output.append('*')
-        
-    def visit_literal(self, node):
-        self.output.append('`')
-        
-    def depart_literal(self, node):
-        self.output.append('`')
-
-##################################################################################
-##################################################################################
-
-def rst2wiki(source):
-	return publish_string(source, writer=WikiWriter())
+from rst2wiki import rst2wiki
 
 class ChangedEditor(delta.Editor):
 	def __init__(self, root, base_root):
@@ -208,6 +80,10 @@ class SvnController:
 		self.rev = rev
 		self.rstfilter = rstfilter
 
+	def get_author(self):
+		author = fs.revision_prop(self.fs_ptr, self.rev, core.SVN_PROP_REVISION_AUTHOR) or ''
+		return author
+
 	def get_changes(self):
 		base_rev = self.rev - 1
 		#get the current root
@@ -244,28 +120,28 @@ class SvnController:
 	them put them to trac
 """
 class Controller:
-	def __init__(self, workpath, repos_path, rev, trac_path, rst_path):
+	def __init__(self, repos_path, rev, trac_path, rst_path):
 		self.rst_path = rst_path
 		self.rev = rev
 		self.trac_path = trac_path
+		self.rst_path = rst_path
 		self.rstfilter = RstFilter(rst_path)
 		self.svnctl = SvnController(repos_path, rev, self.rstfilter)
 		self.env = trac.env.Environment(self.trac_path)
 		self.wikiadmin = trac.wiki.admin.WikiAdmin(self.env)
 		self.wikisystem = trac.wiki.api.WikiSystem(self.env)
-		self.workpath = workpath
+		tempfile.tempdir = "/tmp"
+		self.author = self.svnctl.get_author()
 
 	def import_wiki(self, filename, replace_flag=False):
 		input = self.svnctl.get_file(filename, self.rev)
 		output = rst2wiki(input)
-		(path, name) = os.path.split(filename)
-		(rstname, ext) = os.path.splitext(name)
-		wikifile = self.workpath + "/" + rstname + ".wiki"
-		wikititle = rstname 
-		fd = open(wikifile, "w")
-		fd.write(output)
-		fd.close()
-		self.wikiadmin.import_page(wikifile, wikititle, replace=replace_flag)
+		name = string.split(filename, self.rst_path)[1]
+		wikititle = ''.join(name.split('/')).split('.')[0]
+
+		page = trac.wiki.model.WikiPage(self.env, wikititle)
+		page.text = output
+		page.save(self.author, "", "127.0.0.1")
 
 	def A_wiki(self, filename):
 		self.import_wiki(filename)
@@ -274,8 +150,8 @@ class Controller:
 		self.import_wiki(filename)
 
 	def D_wiki(self, filename):
-		name = os.path.split(filename)[1]
-		wikititle = os.path.splitext(name)[0]
+		name = string.split(filename, self.rst_path)[1]
+		wikititle = ''.join(name.split('/')).split('.')[0]
 		page = trac.wiki.model.WikiPage(self.env, wikititle)
 		page.delete()
 
@@ -286,10 +162,11 @@ class Controller:
 			if elem[0] in ['A', 'D', 'U']:
 				getattr(self, elem[0] + '_wiki')(elem[1])
 
-
 if __name__ == "__main__":
 	repos_path = sys.argv[1]
 	rev = int(sys.argv[2])
-	workpath = sys.argv[3]
-	ctl = Controller(workpath, repos_path, rev, trac_path, rst_path)
+	trac_path = sys.argv[3]
+	rst_path = sys.argv[4]
+
+	ctl = Controller(repos_path, rev, trac_path, rst_path)
 	ctl.main()
